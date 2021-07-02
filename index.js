5d57d9ea8cd415106eebc614436998f6946ac1fd
Michael Salvagna <michael.salvagna@truefort.com>
2021-07-02 12:09:25 -0400

    Forced node to include drilldown files in 7.7

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ module.exports = function (kibana) {
         'plugins/kibana-plugin-drilldownmenu/field_format_editor/drill_down',
       ],
       hacks: [
+        'plugins/kibana-plugin-drilldownmenu/field_format/drill_down',
+        'plugins/kibana-plugin-drilldownmenu/field_format_editor/drill_down',
         'plugins/kibana-plugin-drilldownmenu/hacks/darkMode',
         'plugins/kibana-plugin-drilldownmenu/hacks/hack',
       ],
